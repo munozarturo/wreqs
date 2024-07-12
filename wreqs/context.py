1913a87864d505392f1b577bbca00dd86790a718
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-12 13:57:35 -0400

    build: add docs

--- wreqs/context.py
+++ wreqs/context.py
@@ -24,10 +24,31 @@ class RequestContext:
         self.check_retry = check_retry
 
     def _fetch(self) -> Response:
+        """
+        Prepare and send the HTTP request.
+
+        This method prepares the request and sends it using the session object.
+
+        Returns:
+            Response: The response received from the server.
+        """
         prepared_request = self.session.prepare_request(self.request)
         return self.session.send(prepared_request)
 
     def _handle_retry(self) -> Response:
+        """
+        Handle the retry logic for the request.
+
+        This method attempts to send the request and retry if necessary, based on
+        the check_retry function and max_retries limit.
+
+        Returns:
+            Response: The successful response after retries.
+
+        Raises:
+            RetryRequestError: If the maximum number of retries is reached without a
+                successful response.
+        """
         retries = 0
         while retries < self.max_retries:
             self.response = self._fetch()
