6e85b887377b9bff72598a80da1f84d46462b9b5
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-17 13:58:32 -0400

    build: added typed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="wreqs",
-    version="1.1.0",
+    version="1.1.1",
     author="Arturo Munoz",
     author_email="munoz.arturoroman@gmail.com",
     description="Simplified and enhanced request handling.",
@@ -14,6 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/munozarturo/wreqs",
     packages=find_packages(),
+    package_data={"wreqs": ["py.typed"]},
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
