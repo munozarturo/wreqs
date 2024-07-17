ab79dbbcdb89e5c9d27bdc937eb3513285cb2d1b
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-17 13:43:12 -0400

    build: add language classifiers

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
